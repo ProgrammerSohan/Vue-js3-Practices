47a06a4b74659349c28b8ff54ad3328a1f5006fa
ProgrammerSohan <sohanofficial2022@gmail.com>
2022-09-29 08:54:46 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+
+  <p>Welcome to vue3</p>
+  <HelloWorldComponent :title="title" />
+  <CardComponent title="Header 1" description="1111 Lorem cupidatat eiusmod Lorem cupidatat eiusmod Lorem cupidatat eiusmod." />
+ <!--
+   <CardComponent title="Header 2" description="2222 Lorem cupidatat eiusmod Lorem cupidatat eiusmod Lorem cupidatat eiusmod. "/>
+  <CardComponent title="Header 3" description="3333 Lorem cupidatat eiusmod Lorem cupidatat eiusmod Lorem cupidatat eiusmod. "/>
+  <CardComponent title="Header 4" description="4444 Lorem cupidatat eiusmod Lorem cupidatat eiusmod Lorem cupidatat eiusmod. "/>
+    <CardComponent title="New Header 1" description="5555 Lorem cupidatat eiusmod Lorem cupidatat eiusmod Lorem cupidatat eiusmod. "/>
+ -->
+    
+    <LayoutComponent>
+
+     
+      <CardComponent title="Header 1" description="1111 Lorem cupidatat eiusmod Lorem cupidatat eiusmod Lorem cupidatat eiusmod. "/>
+      
+     
+      <template v-slot:afterFooter>
+        <p>&copy; Maestro</p>
+
+      </template >
+
+    </LayoutComponent>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="js">
+import HelloWorldComponent from './components/HelloWorldComponent.vue';
+import CardComponent from './components/CardComponent.vue';
+import LayoutComponent from './components/LayoutComponent.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorldComponent,
+    CardComponent,
+    LayoutComponent
+
+  },
+  data(){
+    return{
+      title: "Vuejs Learning 4"
+    }
   }
 }
 </script>
 
-<style>
+<style lang="css">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/LayoutComponent.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <div>
+        <p class="header">Header</p>
+
+            <slot>No Component Added.</slot>
+        <p class="footer">Footer</p>
+        <slot name="afterFooter"></slot>
+        
+    </div>
+</template>
+<script lang="js">
+export default {
+     name: "LayoutComponent"   
+
+}
+</script>
+<style lang="css">
+.header,
+.footer{
+    background-color: black;
+    color:white;
+    padding: 20px;
+
+}
+
+    
+</style>
